6811289c790b06fd592bff70223e30886b8f212f
yEsPaP <csgo40055@gmail.com>
2020-03-19 10:15:48 +0200

    auto select mali by android version

--- build.sh
+++ build.sh
@@ -10,8 +10,10 @@ read -p "Enter android build version: " major
 if [major == "q"]
 then
 export ANDROID_MAJOR_VERSION=q
+echo -e "CONFIG_DDK_VERSION_OS="q"\nCONFIG_MALI_R29P0=y" >> $defconfig
 else if [major == "p"]
 export ANDROID_MAJOR_VERSION=p
+echo -e "CONFIG_DDK_VERSION_OS="p"\nCONFIG_MALI_R28P0=y" >> $defconfig
 fi
 
 jobs=$(grep -c '^processor' /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
