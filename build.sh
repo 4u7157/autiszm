05321e140eab0f897d0c4ad976c40dffb1cd15eb
yEsPaP <csgo40055@gmail.com>
2020-03-19 10:49:15 +0200

    fix script

--- build.sh
+++ build.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-echo -e "Cleaning\n"
-make clean
-make mrproper
-rm arch/arm64/boot/dtb
-rm arch/arm64/boot/dts/*.dtb
+echo -e "Cleaning"
+make clean &> /dev/null
+make mrproper &> /dev/null
+rm arch/arm64/boot/dtb &> /dev/null
+rm arch/arm64/boot/dts/*.dtb &> /dev/null
 
 export ARCH=arm64
 export CROSS_COMPILE=../toolchain/bin/aarch64-linux-gnu-
+export USE_CCACHE=1
 
 read -p "Enter defconfig name: " defconfig
 
 read -p "Enter android build version: " major
 
-if [major == "q"]
-then
-echo "Build Q kernel"
+if [ "$major" == "q" ]; then
+echo "Building Q kernel"
 export ANDROID_MAJOR_VERSION=q
-echo -e "CONFIG_DDK_VERSION_OS="q"\nCONFIG_MALI_R29P0=y" >> $defconfig
-else if [major == "p"]
-echo "Build P kernel"
+echo -e "CONFIG_DDK_VERSION_OS="q"\nCONFIG_MALI_R29P0=y" >> arch/arm64/configs/$defconfig
+else if [ "$major" == "p" ]; then
+echo "Building P kernel"
 export ANDROID_MAJOR_VERSION=p
-echo -e "CONFIG_DDK_VERSION_OS="p"\nCONFIG_MALI_R28P0=y" >> $defconfig
+echo -e "CONFIG_DDK_VERSION_OS="p"\nCONFIG_MALI_R28P0=y" >> arch/arm64/configs/$defconfig
 fi
 
 ## Core count check
-jobs=$(nproc -all)
+jobs=$(nproc --all)
 
 ## dtb
 make $defconfig
 make exynos7870-j7xelte_eur_open_00.dtb exynos7870-j7xelte_eur_open_01.dtb exynos7870-j7xelte_eur_open_02.dtb exynos7870-j7xelte_eur_open_03.dtb exynos7870-j7xelte_eur_open_04.dtb
 ./tools/dtbtool arch/arm64/boot/dts/ -o arch/arm64/boot/dtb
 
+## Image
 make $defconfig
 make -j$(jobs)
