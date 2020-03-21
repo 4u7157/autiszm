df7ab7b3582ecff2075befbc5480ca8468db0776
yEsPaP <csgo40055@gmail.com>
2020-03-21 10:28:35 +0200

    add all devices

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ make clean &> /dev/null
 make mrproper &> /dev/null
 rm arch/arm64/boot/dtb &> /dev/null
 rm arch/arm64/boot/dts/*.dtb &> /dev/null
-rm arch/arm64/kernel/vdso/vdso.lds
-rm arch/arm64/kernel/vdso/vdso.so
-rm arch/arm64/kernel/vdso/vdso.so.dbg
-rm arch/arm64/kernel/vmlinux.lds
-rm security/samsung/defex_lsm/cert/pubkey_eng.der
-rm security/samsung/defex_lsm/cert/pubkey_user.der
-rm security/samsung/defex_lsm/defex_packed_rules.bin
+rm arch/arm64/kernel/vdso/vdso.lds &> /dev/null
+rm arch/arm64/kernel/vdso/vdso.so &> /dev/null
+rm arch/arm64/kernel/vdso/vdso.so.dbg &> /dev/null
+rm arch/arm64/kernel/vmlinux.lds &> /dev/null
+rm security/samsung/defex_lsm/cert/pubkey_eng.der &> /dev/null
+rm security/samsung/defex_lsm/cert/pubkey_user.der &> /dev/null
+rm security/samsung/defex_lsm/defex_packed_rules.bin &> /dev/null
 
 ############################################################# exports
 export ARCH=arm64
@@ -25,6 +25,12 @@ read -p "Enter android build version: " major
 
 ############################################################# b1g memes here W.I.P
 if [ "$defconfig" == "exynos7870-j7xelte_defconfig" ]; then
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+
+else if [ "$defconfig" == "exynos7870-gtaxllte_defconfig" ]; then
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+
+else if [ "$defconfig" == "exynos7870-gtaxlwifi_defconfig" ]; then
 read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 else if [ "$defconfig" == "exynos7870-j7velte_defconfig" ]; then
@@ -52,20 +58,20 @@ read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " 
 
 
 ## these don't need dtb change because they are always treble, but they need USB fix for AOSP
+else if [ "$defconfig" == "exynos7870-a2corelte_defconfig" ]; then
+read -p "AOSP(1) or OneUI(3): " type
 else if [ "$defconfig" == "exynos7870-a6lte_defconfig" ]; then
 read -p "AOSP(1) or OneUI(3): " type
 else if [ "$defconfig" == "exynos7870-j6lte_defconfig" ]; then
 read -p "AOSP(1) or OneUI(3): " type
 else if [ "$defconfig" == "exynos7870-m10lte_defconfig" ]; then
 read -p "AOSP(1) or OneUI(3): " type
-fi
 
 ############################################################# Mali version fix
 if [ "$major" == "p" ]; then
 export ANDROID_MAJOR_VERSION=p
 sed -i 's/CONFIG_DDK_VERSION_OS="q"/"CONFIG_DDK_VERSION_OS="p"/g' arch/arm64/configs/$defconfig
 sed -i 's/CONFIG_MALI_R29P0=y/CONFIG_MALI_R28P0=y/g' arch/arm64/configs/$defconfig
-fi
 
 ############################################################# Treble USB Fix
 if [ "$type" == "1" ]; then
@@ -84,26 +90,24 @@ if [ "$type" == "1" ]; then
 sed -i 's/# CONFIG_USB_ANDROID_SAMSUNG_MTP is not set/CONFIG_USB_ANDROID_SAMSUNG_MTP=y/g' arch/arm64/configs/$defconfig
 else if [ "$type" == "2" ]; then
 sed -i 's/# CONFIG_USB_ANDROID_SAMSUNG_MTP is not set/CONFIG_USB_ANDROID_SAMSUNG_MTP=y/g' arch/arm64/configs/$defconfig
-fi
 
 ############################################################# Mali version fix restore
 if [ "$major" == "p" ]; then
 sed -i 's/CONFIG_DDK_VERSION_OS="p"/"CONFIG_DDK_VERSION_OS="q"/g' arch/arm64/configs/$defconfig
 sed -i 's/CONFIG_MALI_R28P0=y/CONFIG_MALI_R29P0=y/g' arch/arm64/configs/$defconfig
-fi
 
 ############################################################# Make source clean, but leave Image && dtb
-echo -e "Cleaning"
+echo -e "Cleaning again"
 make clean &> /dev/null
 make mrproper &> /dev/null
 rm arch/arm64/boot/dts/*.dtb &> /dev/null
-rm arch/arm64/kernel/vdso/vdso.lds
-rm arch/arm64/kernel/vdso/vdso.so
-rm arch/arm64/kernel/vdso/vdso.so.dbg
-rm arch/arm64/kernel/vmlinux.lds
-rm security/samsung/defex_lsm/cert/pubkey_eng.der
-rm security/samsung/defex_lsm/cert/pubkey_user.der
-rm security/samsung/defex_lsm/defex_packed_rules.bin
+rm arch/arm64/kernel/vdso/vdso.lds &> /dev/null
+rm arch/arm64/kernel/vdso/vdso.so &> /dev/null
+rm arch/arm64/kernel/vdso/vdso.so.dbg &> /dev/null
+rm arch/arm64/kernel/vmlinux.lds &> /dev/null
+rm security/samsung/defex_lsm/cert/pubkey_eng.der &> /dev/null
+rm security/samsung/defex_lsm/cert/pubkey_user.der &> /dev/null
+rm security/samsung/defex_lsm/defex_packed_rules.bin &> /dev/null
 ############################################################# end
 fi
 fi
@@ -117,4 +121,10 @@ fi
 fi
 fi
 fi
-
+fi
+fi
+fi
+fi
+fi
+fi
+fi
