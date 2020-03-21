061a4177507918991038832e3b1ee95b2e322e5d
yEsPaP <csgo40055@gmail.com>
2020-03-21 11:54:03 +0200

    script almost done

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo -e "Cleaning"
 make clean &> /dev/null
 make mrproper &> /dev/null
-rm arch/arm64/boot/dtb &> /dev/null
+rm -rf output/
 rm arch/arm64/boot/dts/*.dtb &> /dev/null
 rm arch/arm64/kernel/vdso/vdso.lds &> /dev/null
 rm arch/arm64/kernel/vdso/vdso.so &> /dev/null
@@ -20,56 +20,55 @@ export USE_CCACHE=1
 ## this works for pp aswell
 export ANDROID_MAJOR_VERSION=q
 
-read -p -r "Enter defconfig name: " defconfig
-read -p -r "Enter android build version: " major
+read -p "Enter defconfig name: " defconfig
+read -p "Enter android build version: " major
 
 ############################################################# b1g memes here W.I.P
 if [ "$defconfig" == "exynos7870-j7xelte_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 elif [ "$defconfig" == "exynos7870-gtaxllte_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 elif [ "$defconfig" == "exynos7870-gtaxlwifi_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 elif [ "$defconfig" == "exynos7870-j7velte_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 
 elif [ "$defconfig" == "exynos7870-on7xelte_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 
 elif [ "$defconfig" == "exynos7870-a3y17lte_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 
 elif [ "$defconfig" == "exynos7870-j5y17lte_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 
 elif [ "$defconfig" == "exynos7870-j7y17lte_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 
 elif [ "$defconfig" == "exynos7870-on7xreflte_defconfig" ]; then
-read -p -r "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
+read -p "Non Treble AOSP(1) or Treble AOSP(2) or OneUI(3) or Treble OneUI(4): " type
 
 
 ## these don't need dtb change because they are always treble, but they need USB fix for AOSP
 elif [ "$defconfig" == "exynos7870-a2corelte_defconfig" ]; then
-read -p -r "AOSP(1) or OneUI(3): " type
+read -p "AOSP(1) or OneUI(3): " type
 elif [ "$defconfig" == "exynos7870-a6lte_defconfig" ]; then
-read -p -r "AOSP(1) or OneUI(3): " type
+read -p "AOSP(1) or OneUI(3): " type
 elif [ "$defconfig" == "exynos7870-j6lte_defconfig" ]; then
-read -p -r "AOSP(1) or OneUI(3): " type
+read -p "AOSP(1) or OneUI(3): " type
 elif [ "$defconfig" == "exynos7870-m10lte_defconfig" ]; then
-read -p -r "AOSP(1) or OneUI(3): " type
-
+read -p "AOSP(1) or OneUI(3): " type
+fi
 ############################################################# Mali version fix
 if [ "$major" == "p" ]; then
-export ANDROID_MAJOR_VERSION=p
 sed -i 's/CONFIG_DDK_VERSION_OS="q"/"CONFIG_DDK_VERSION_OS="p"/g' arch/arm64/configs/"$defconfig"
 sed -i 's/CONFIG_MALI_R29P0=y/CONFIG_MALI_R28P0=y/g' arch/arm64/configs/"$defconfig"
 
@@ -98,6 +97,9 @@ sed -i 's/CONFIG_MALI_R28P0=y/CONFIG_MALI_R29P0=y/g' arch/arm64/configs/"$defcon
 
 ############################################################# Make source clean, but leave Image && dtb
 echo -e "Cleaning again"
+mkdir output/
+mv arch/arm64/boot/Image output/
+mv arch/arm64/boot/dtb output/
 make clean &> /dev/null
 make mrproper &> /dev/null
 rm arch/arm64/boot/dts/*.dtb &> /dev/null
@@ -109,7 +111,6 @@ rm security/samsung/defex_lsm/cert/pubkey_eng.der &> /dev/null
 rm security/samsung/defex_lsm/cert/pubkey_user.der &> /dev/null
 rm security/samsung/defex_lsm/defex_packed_rules.bin &> /dev/null
 ############################################################# end
-fi
 fi
 fi
 fi
