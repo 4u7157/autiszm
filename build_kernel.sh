9e606accf73ef205cb8897a1e2e67725a11ec760
yEsPaP <csgo40055@gmail.com>
2020-04-16 08:38:29 +0000

    clanggit status

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=q
 
-make exynos7870-a6lte_defconfig
-make -j64
+make ARCH=arm64 exynos7870-a6lte_defconfig
+
+PATH="../clang/bin:../toolchain/bin:${PATH}" \
+make -j$(nproc --all) \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CROSS_COMPILE=aarch64-gnu-
