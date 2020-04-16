f48d2c41ca106dd409a4095a5b96f1db349cf7fe
yEsPaP <csgo40055@gmail.com>
2020-04-16 10:03:01 +0000

    switch compiler

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,8 +4,8 @@ export ANDROID_MAJOR_VERSION=q
 
 make ARCH=arm64 exynos7870-a6lte_defconfig
 
-PATH="../clang/bin:../toolchain/bin:${PATH}" \
+PATH="../clang/bin:../tc1/bin:${PATH}" \
 make -j$(nproc --all) \
                       ARCH=arm64 \
                       CC=clang \
-                      CROSS_COMPILE=aarch64-gnu-
+                      CROSS_COMPILE=aarch64-linux-gnu-
