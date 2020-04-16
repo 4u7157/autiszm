392991741c697d35d504814584ef58ee14890e9b
yEsPaP <csgo40055@gmail.com>
2020-04-16 14:37:31 +0000

    O3

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../toolchain/aarch64-unknown-linux-gnu-
+export CROSS_COMPILE=../toolchain/bin/aarch64-unknown-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
 
 make exynos7870-a6lte_defconfig
