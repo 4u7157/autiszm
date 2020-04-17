9f36594801f9c98ae17fe262a703d031a112c2fd
yEsPaP <csgo40055@gmail.com>
2020-04-17 16:45:48 +0000

    switch target to j7xelte

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,6 @@
 
 export ARCH=arm64
 export CROSS_COMPILE=../toolchain/bin/aarch64-unknown-linux-gnu-
-export ANDROID_MAJOR_VERSION=q
 
-make exynos7870-a6lte_defconfig
+make exynos7870-j7xlte_defconfig
 make -j8
