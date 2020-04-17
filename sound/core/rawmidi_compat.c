a5ede04efa0824f91a083eb2737f2608b49acb0c
yEsPaP <csgo40055@gmail.com>
2020-04-17 16:25:26 +0300

    Update rawmidi_compat.c

--- sound/core/rawmidi_compat.c
+++ sound/core/rawmidi_compat.c
@@ -61,7 +61,7 @@ struct snd_rawmidi_status32 {
 	u32 avail;
 	u32 xruns;
 	unsigned char reserved[16];
-} __attribute__((packed));
+} __attribute__((packed, aligned(2)));
 
 static int snd_rawmidi_ioctl_status_compat(struct snd_rawmidi_file *rfile,
 					   struct snd_rawmidi_status32 __user *src)
