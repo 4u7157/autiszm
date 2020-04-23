d173b370fc1307efdae5469b475bd81c69af7a2a
yEsPaP <csgo40055@gmail.com>
2020-04-23 08:41:37 +0000

    build dtb

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,5 +3,9 @@
 export ARCH=arm64
 export CROSS_COMPILE=../toolchain/bin/aarch64-linux-gnu-
 
+dtbfiles="exynos7870-j7xelte_eur_open_00.dtb exynos7870-j7xelte_eur_open_01.dtb exynos7870-j7xelte_eur_open_02.dtb exynos7870-j7xelte_eur_open_03.dtb exynos7870-j7xelte_eur_open_04.dtb"
+
 make exynos7870-j7xelte_defconfig
-make -j8
+make "$dtbfiles"
+./tools/dtbtool arch/arm64/boot/dts/ -o arch/arm64/boot/dtb
+make -j"$(nproc --all)"
